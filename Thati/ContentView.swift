ad9626debaecf93ecc1cf51c496dc93be677c5d5
Reema <Reemabf2@gmail.com>
2023-10-11 17:41:23 +0300

    Initial Commit

---
+++ Thati/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Thati
+//
+//  Created by Reema Alfaleh on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Thati/ThatiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThatiApp.swift
+//  Thati
+//
+//  Created by Reema Alfaleh on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ThatiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
