8f779cbc776ce0860fea314c1086c7b2325b8e99
Ja1y1 <146494173+Ja1y1@users.noreply.github.com>
2023-10-12 17:01:38 +0300

    demoview for video

---
+++ Thati/views/demoView.swift
@@ -0,0 +1,18 @@
+//
+//  demoView.swift
+//  Thati
+//
+//  Created by JAY on 12/10/2023.
+//
+
+import SwiftUI
+
+struct demoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    demoView()
+}
